aab66cdb972bd537bbf82614083d30247dbf620b
Alan Wake <shitikov_1910@mail.ru>
2024-02-21 01:17:30 +0300

    removed grabs

--- Content.Shared/SS220/Grab/GrabStateEvents.cs
+++
@@ -1,6 +0,0 @@
-// © SS220, An EULA/CLA with a hosting restriction, full text: https://raw.githubusercontent.com/SerbiaStrong-220/space-station-14/master/CLA.txt
-
-namespace Content.Shared.SS220.Grab;
-
-public sealed class GrabStartEvent : EntityEventArgs {};
-public sealed class GrabEndEvent : CancellableEntityEventArgs {};
