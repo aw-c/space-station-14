9d69b456b67999d5b31a5fe5b87725e1551d2f3c
Alan Wake <shitikov_1910@mail.ru>
2024-03-25 09:31:58 +0300

    additional missable in combatmode

--- Content.Shared/SS220/CQC/CQCCombatSystemShared.cs
+++ Content.Shared/SS220/CQC/CQCCombatSystemShared.cs
@@ -1,6 +1,7 @@
 // © SS220, An EULA/CLA with a hosting restriction, full text: https://raw.githubusercontent.com/SerbiaStrong-220/space-station-14/master/CLA.txt
 
 using Content.Shared.SS220.Missable;
+using Content.Shared.CombatMode;
 
 namespace Content.Shared.SS220.CQCCombat;
 public partial class CQCCombatSharedSystem : EntitySystem
@@ -13,6 +14,11 @@ public partial class CQCCombatSharedSystem : EntitySystem
 
     private void RollChanceToMiss(EntityUid entity, CQCCombatComponent comp, MissableMissChanceBonusEvent args)
     {
+        if (!TryComp<CombatModeComponent>(entity, out var combat))
+            return;
+        if (!combat.IsInCombatMode)
+            return;
+
         args.Handled = true;
         args.BonusMiss.Add(MissChance);
     }
